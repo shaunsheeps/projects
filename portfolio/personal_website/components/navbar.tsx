c4bfb50484b180219d49ad97b73d211f2d5a8518
shaunsheeps <97936736+shaunsheeps@users.noreply.github.com>
2024-05-15 17:26:30 -0700

    Created basic structure for website

---
+++ portfolio/personal_website/components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+    return (
+        <Navbar className=''>
+            
+        </Navbar>
+    );
+}
+
+export default NavBar;
